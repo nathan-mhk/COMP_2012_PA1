596f00928ecb3f14f3e94f1e37ed0c1fbed21c5a
NRVulture <51946365+NRVulture@users.noreply.github.com>
2019-09-22 18:58:40 +0800

    Completed Restaurant.cpp
    Pending for debug

--- Source/restaurant.cpp
+++ Source/restaurant.cpp
@@ -20,27 +20,44 @@ bool Restaurant::add_meal(Food food) {
 	} else {
 		this->list_end_index = (this->list_end_index + 1) % 5;
 		this->meals_list[this->list_end_index] = food;
-		this->curr_num_of_meals++;
+		++this->curr_num_of_meals;
 		return true;
 	}
 }
 
 bool Restaurant::remove_first_meal(void) {
-	if (!this->curr_num_of_meals) {
+	if (!curr_num_of_meals) {
 		return false;
 	} else {
+		list_start_index = (list_start_index + 1) % 5;
+		--curr_num_of_meals;
 		return true;
 	}
 }
 
 bool Restaurant::remove_last_meal(void) {
-	if (!this->curr_num_of_meals) {
+	if (!curr_num_of_meals) {
 		return false;
 	} else {
+		list_end_index = (list_end_index + 1) % 5;
+		--curr_num_of_meals;
 		return true;
 	}
 }
 
 bool Restaurant::serve_meal(Buddy* buddy, int meal_index) const {
-	return true;
+	Food food = this->meals_list[meal_index];
+	int money = buddy->get_money() - food.get_price();
+
+	if (money >= 0) {
+		int fat = buddy->get_fat() + food.get_fat_gain();
+		int muscle = buddy->get_muscle() + food.get_protein_gain();
+
+		buddy->set_money(money);
+		buddy->set_fat(fat);
+		buddy->set_muscle(muscle);
+		return true;
+	} else {
+		return false;
+	}
 }
